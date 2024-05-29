2439655fe3f144d7683d0680158e2e4fed67be64
M Huzaifa Nizami <mhuzaifanizami@gmail.com>
2024-05-29 11:04:48 +0500

    file updated

---
+++ app.js
@@ -0,0 +1,45 @@
+// Q NO 01 
+// . Show an alert box on click on a link
+
+// function showAlert() {
+//     alert("This is an alert box")
+
+// }
+
+// Q no 02 
+// Display some Mobile images in browser. On click on an
+// image Show the message in alert to user.
+
+// function showAlert(){
+//  alert("Thanks for Purchasing from us")
+// }
+
+// Q no 03 
+// Display 10 student records in table and each row should contain a delete button. If you click on a button to delete a record, entire row should be deleted.
+
+// function deleteRow(button) {
+//     var row = button.parentNode.parentNode;
+//     row.parentNode.removeChild(row)
+// }
+
+// Q no 4 
+// Display an image in browser. Change the picture on mouseover and set the first picture on mouseout
+
+// Q no 05 
+// . Show a counter in browser. Counter should increase on click on increase button and decrease on click on decrease button. And show updated counter value in browser
+
+// var count = 0;
+
+// function increaseValue(){
+//     count++;
+//     console.log(count)
+//     updateDisplay()
+// }
+// function decreaseValue(){
+//     count--;
+//     console.log(count)
+//     updateDisplay()
+// }
+// function updateDisplay(){
+//     document.getElementById("counter").innerText = count;
+// }
